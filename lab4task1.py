9a393e7d325af872b10ccead67a6a4c032cb4489
Zixuan Liu <zx572344@dal.ca>
2020-05-11 15:46:25 -0300

    Add Task2

--- lab4task1.py
+++ lab4task1.py
@@ -1,13 +1,29 @@
 import string
 
-punc = string.punctuation
+punc = string.punctuation + string.whitespace
 
 file = open("book.txt",mode="r")
 
+count = 0
+word_dict = {}
+res = []
+
 for line in file:
 	tmp = line.split(" ")
+	count += len(tmp)
 	for word in tmp:
 		word = word.strip(punc)
 		word = word.lower()
-		print(word, end=" ")
-	print()
+		if word_dict.get(word) is None:
+			word_dict[word] = 1
+		else:
+			word_dict[word] += 1
+
+for k,v in word_dict.items():
+	res.append((v,k))
+res.sort(reverse=True)
+
+for i in res[:20]:
+	print(i[1], i[0])
+
+print("The # of different words used in the book is", len(word_dict))
