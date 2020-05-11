e02898052a284877f736660d799f03e051c0500b
Zixuan Liu <zx572344@dal.ca>
2020-05-11 18:30:38 -0300

    add Homework Q1 part2 Python Set Subtraction

--- Homework-Q1.py
+++ Homework-Q1.py
@@ -8,11 +8,13 @@ word = [i for i in word]
 word_dict = {}
 
 res = []
+set_book = set()
 for line in file:
 	tmp = line.split(" ")
 	for w in tmp:
 		w = w.strip(punc)
 		w = w.lower()
+		set_book.add(w)
 		if word_dict.get(w) is None:
 			word_dict[w] = 1
 		else:
@@ -23,3 +25,13 @@ for k,v in word_dict.items():
 for i in res:
 	print(i)
 print("The # of words not in words.txt is", len(res)) 
+
+print()
+
+# Q1 Part2 Use Python Set
+
+set_word = set(word)
+set_ans = set_book - set_word
+for i in set_ans:
+	print(i)
+print("Use Python Set, the difference is",len(set_ans))
