9fcb72073106e49d9600a1512b0df59212b78780
Zixuan Liu <zx572344@dal.ca>
2020-05-11 22:27:10 -0300

    Add Homework-Q2 part2 use md5 checksum

--- Homework-Q2.py
+++ Homework-Q2.py
@@ -1,7 +1,21 @@
+"""
+os.path learned from GeeksforGeeks.com
+URL: https://www.geeksforgeeks.org/os-path-module-python/
+Date: May 11, 2020
+Author: GeeksforGeeks.com
+
+MD5 Checksum learned from Stackoverflow.com
+URL: https://stackoverflow.com/questions/3431825/generating-an-md5-checksum-of-a-file
+Date: May 11, 2020
+Author: Stackoverflow.com
+"""
+
 import os
+import hashlib
 
 ans = []
-
+files = {}
+res = []
 
 def searchMP3(dirname, res):
     for name in os.listdir(dirname):
@@ -12,9 +26,30 @@ def searchMP3(dirname, res):
         else:
             searchMP3(pa, res)
 
+def check_duplicates(ans):
+	for i in ans:
+		checksum = hashlib.md5(open(i, "br").read()).hexdigest()   # use hashlib.md5 compute checksum for each file
+		if files.get(checksum) is None:    # use dictionary to record, key = checksum, value = path
+			files[checksum] = [i]
+		else:
+			files[checksum].append(i)
+	for k,v in files.items():
+		if len(v) > 1:
+			res.append(v)
+	return res
 
-searchMP3("Your_Absolute_Path", ans)
 
-for i in ans:
-    print(i)
+
+searchMP3("your_absolute_path", ans)
+
+#for i in ans:
+#    print(i)
+
+result = check_duplicates(ans)
+for i in result:
+	for j in i:
+		print(j)
+	print()
+
+
 
